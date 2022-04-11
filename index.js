d0c454030a7e72c14a756bda4cd600ddcc74e048
Clement GENAY <clement@gosnat.com>
2022-04-11 16:35:35 +0200

    Ajout v1 lecture de fichier CSV

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.get('/upload', function(request, response) {
     response.render('uploadFile.html');
 });
 
+/*
 // Give URL for getting a request
 app.post('/uploadCsv', uploadFileController.uploadCsv);
 
 // Create the function for apload
-var fs = require("fs");
 module.exports.uploadCsv = function(req, res) {
             var today = new Date();
             var date = today.getDate();
@@ -41,3 +41,23 @@ module.exports.uploadCsv = function(req, res) {
                         console.log("Upload completed!");
             });
 };
+*/
+//lecture de fichier selon le departement
+const csv = require('csv-parser');
+
+app.get('/read/:dep',function(request,response){
+
+response.write('<html><head></head><body><table><thead><tr><th> Date</th><th>Departement</th><th>Reanimation</th><th>Hospitalisation</th></tr></thead>');
+fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+  if (row.lib_dep==request.params.dep){
+  response.write('<tr><td>'+row.date+'</td><td>'+row.lib_dep+'</td><td>'+row.rea+'</td><td>'+row.hosp+'</td></tr>');
+  
+}
+})
+  response.write('</body></html>');
+  
+
+
+})  
