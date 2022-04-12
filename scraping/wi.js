1240faf7caa263d7fc575b015dea79a1ef77f2bb
clemence17 <clemence.core@laposte.net>
2022-04-12 16:47:11 +0200

    update

--- scraping/wi.js
+++ scraping/wi.js
@@ -19,12 +19,19 @@ app.get('/covid_countries',function (request, response) {
                 accept:
                 "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                 "accept-encoding": "gzip, deflate, br",
+<<<<<<< HEAD
                 "accept-language": "en-US,en;q=0.9"
+=======
+                "accept-language": "fr,en-US,en;q=0.9"
+>>>>>>> ed8fc7faae3ce884a190bcced60764e3c2dfa447
             },
             gzip: true,
         }).then(function (html) {
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> ed8fc7faae3ce884a190bcced60764e3c2dfa447
             let $ = cheerio.load(html);
             let donnees = [];
             let donnees2 = [];
@@ -32,6 +39,7 @@ app.get('/covid_countries',function (request, response) {
 
             let rows = $('table.wikitable tbody tr').each((idx, elem) => {
                 rank =$(elem).find('th').text().replace(/[\n\r]+/g,'');
+<<<<<<< HEAD
         //name = $(elem).find('td a').html();
         donnees2 = [];
         cols = $(elem).find('td').each((colidx, colelem) => {
@@ -68,6 +76,7 @@ app.get('/covid_countries',function (request, response) {
     response.send('CSV créé');
 })
 
+<<<<<<< HEAD
 }
 )();
 
@@ -77,4 +86,8 @@ response.send('CSV créé sur les données de pandémie mondial');
 
 app.listen(PORT, function(){
     console.log('Bienvenue sur les données du coronavirus : ' + PORT);
+=======
+app.listen(PORT, function(){
+    console.log('Hello : ' + PORT);
+>>>>>>> ed8fc7faae3ce884a190bcced60764e3c2dfa447
 })
