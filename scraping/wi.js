312fb044049a02e8bfff42681d9cf187823c7aa0
clemence17 <clemence.core@laposte.net>
2022-04-12 16:44:31 +0200

    update

--- scraping/wi.js
+++ scraping/wi.js
@@ -1,31 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 var req = require("request-promise"),
 cheerio = require("cheerio"),
 fs = require("fs"),
 json2csv = require("json2csv").Parser;
 
-// page wikipedia 
-const wiki = "https://fr.wikipedia.org/wiki/Coronavirus";
+app.get('/covid_countries',function (request, response) {
 
-(async () => {
-const response = await req({
-    uri: wiki,
-    headers: {
-        accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-US,en;q=0.9"
-    },
-    gzip: true,
-}).then(function (html) {
+    // page wikipedia 
+    const wiki = "https://fr.wikipedia.org/wiki/Coronavirus";
+
+    (async () => {
+        const response = await req({
+            uri: wiki,
+            headers: {
+                accept:
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9"
+            },
+            gzip: true,
+        }).then(function (html) {
 
 
-    let $ = cheerio.load(html);
-    let donnees = [];
-    let donnees2 = [];
-    let name, rank, cols, col;
+            let $ = cheerio.load(html);
+            let donnees = [];
+            let donnees2 = [];
+            let name, rank, cols, col;
 
-    let rows = $('table.wikitable tbody tr').each((idx, elem) => {
-        rank =$(elem).find('th').text().replace(/[\n\r]+/g,'');
+            let rows = $('table.wikitable tbody tr').each((idx, elem) => {
+                rank =$(elem).find('th').text().replace(/[\n\r]+/g,'');
         //name = $(elem).find('td a').html();
         donnees2 = [];
         cols = $(elem).find('td').each((colidx, colelem) => {
@@ -50,3 +56,11 @@ const response = await req({
 
 }
 )();
+
+response.send('CSV créé sur les données de pandémie mondial');
+
+})
+
+app.listen(PORT, function(){
+    console.log('Bienvenue sur les données du coronavirus : ' + PORT);
+})
