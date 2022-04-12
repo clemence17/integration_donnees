ed8fc7faae3ce884a190bcced60764e3c2dfa447
Aloys <aloys.vernhet@gmail.com>
2022-04-12 16:42:46 +0200

    wi.js update
    Mise à jour du js pour le parsing

--- scraping/wi.js
+++ scraping/wi.js
@@ -1,52 +1,60 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 var req = require("request-promise"),
 cheerio = require("cheerio"),
 fs = require("fs"),
 json2csv = require("json2csv").Parser;
 
-// page wikipedia 
-const wiki = "https://fr.wikipedia.org/wiki/Coronavirus";
+app.get('/covid_countries',function (request, response) {
 
-(async () => {
-const response = await req({
-    uri: wiki,
-    headers: {
-        accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-US,en;q=0.9"
-    },
-    gzip: true,
-}).then(function (html) {
+    // page wikipedia 
+    const wiki = "https://fr.wikipedia.org/wiki/Coronavirus";
 
+    (async () => {
+        const response = await req({
+            uri: wiki,
+            headers: {
+                accept:
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "fr,en-US,en;q=0.9"
+            },
+            gzip: true,
+        }).then(function (html) {
 
-    let $ = cheerio.load(html);
-    let donnees = [];
-    let donnees2 = [];
-    let name, rank, cols, col;
+            let $ = cheerio.load(html);
+            let donnees = [];
+            let donnees2 = [];
+            let name, rank, cols, col;
 
-    let rows = $('table.wikitable tbody tr').each((idx, elem) => {
-        rank =$(elem).find('th').text().replace(/[\n\r]+/g,'');
-        //name = $(elem).find('td a').html();
-        donnees2 = [];
-        cols = $(elem).find('td').each((colidx, colelem) => {
-            col = $(colelem).text().replace(/[\n\r]+/g,'');
-            donnees2.push(col,);    
-        }); 
+            let rows = $('table.wikitable tbody tr').each((idx, elem) => {
+                rank =$(elem).find('th').text().replace(/[\n\r]+/g,'');
+                //name = $(elem).find('td a').html();
+                donnees2 = [];
+                cols = $(elem).find('td').each((colidx, colelem) => {
+                    col = $(colelem).text().replace(/[\n\r]+/g,'');
+                    donnees2.push(col,);
+                });
+                donnees.push({
+                    rank,
+                    ...donnees2,
+                });
+            });
 
-        donnees.push({
-            rank,
-            ...donnees2,
+            // exporting to csv
+            const j2cp = new json2csv()
+            const csv = j2cp.parse(donnees);
+
+            fs.writeFileSync("./wiki.csv", csv, "utf-8");
+        }).catch(function (err) {
+            console.log(err);
         });
-    });
+    })();
+    response.send('CSV créé');
+})
 
-    // exporting to csv
-    const j2cp = new json2csv()
-    const csv = j2cp.parse(donnees);
-
-    fs.writeFileSync("./wiki.csv", csv, "utf-8");
-}).catch(function (err) {
-    console.log(err);
-});
-
-}
-)();
+app.listen(PORT, function(){
+    console.log('Hello : ' + PORT);
+})
