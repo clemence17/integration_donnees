4a8b867f2e021fc150fa539698ddc8180a1f910f
Aloys <aloys.vernhet@gmail.com>
2022-04-14 09:10:15 +0200

    Ajout du scrapping à index.js

--- index.js
+++ index.js
@@ -132,3 +132,72 @@ app.get('/COVID', function(request, response){
 })
 
 
+////////////////////////////:PARTIE SCRAPING
+
+var req = require("request-promise");
+var cheerio = require("cheerio");
+
+app.get('/covid_countries',function (request, response) {
+
+    // page wikipedia 
+    const wiki = "https://fr.wikipedia.org/wiki/Coronavirus";
+
+    (async () => {
+        const response = await req({
+            uri: wiki,
+            headers: {
+                accept:
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "fr,en-US,en;q=0.9"
+            },
+            gzip: true,
+        }).then(function (html) {
+
+            let $ = cheerio.load(html);
+            let donnees = [];
+            let donnees2 = [];
+            let name, rank, cols, col;
+
+            let rows = $('table.wikitable tbody tr').each((idx, elem) => {
+                if (col == "Guam") {
+                    return false
+                } else {
+                rank =$(elem).find('th').text().replace(/[\n\r]+/g,'');
+                donnees2 = [];
+                cols = $(elem).find('td').each((colidx, colelem) => {
+                    col = $(colelem).text().replace(/[\n\r]+/g,'');
+                    if (col == "Guam") {
+                        return false;
+                    } else {
+                        donnees2.push(col,);
+                    }
+                });
+                donnees.push({
+                    rank,
+                    ...donnees2,
+                });
+            }
+            });
+
+            // exporting to json
+            const json = JSON.stringify(donnees);
+
+            fs.writeFileSync("./wikipedia_virus.json", json, "utf-8");
+        }).catch(function (err) {
+            console.log(err);
+        });
+    })();
+    //response.write('json créé sur les données de pandémie mondial');
+
+
+    fs.readFile('wikipedia_virus.json', 'utf-8', (error, data) => {
+        if(error){
+        console.log(error);
+        return;
+        }
+        response.json(JSON.parse(data));
+        //console.log(JSON.parse(data));
+
+    })
+})
